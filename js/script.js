b820bd63d46d9cfb05dae3553ac59263993e7200
andreasansica <sansica.andrea99@gmail.com>
2020-12-09 15:44:10 +0100

    struttura dati header

--- js/script.js
+++ js/script.js
@@ -4,6 +4,29 @@
 var app = new Vue ({
   el: '#app',
   data:{
-
+    superiore:[
+      {
+        telefono: '1.800.567.8910',
+        contatti: 'Contact us',
+        alunni: 'Alumni',
+        campus: 'Campus',
+        facebook:'fab fa-facebook-f',
+        twitter:'fab fa-twitter',
+        youtube:'fab fa-youtube',
+        instagram:'fab fa-instagram'
+      }
+    ],
+    inferiore:[
+      {
+        logo:'img/university_logo.png',
+        home: 'HOME',
+        about: 'ABOUT',
+        corsi: 'COURSES',
+        eventi: 'EVENTS',
+        strutture: 'FACILITIES',
+        novita: 'NEWS',
+        ammissione: 'ADMISSION'
+      }
+    ]
   }
 })
