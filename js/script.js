293376e288604c3ba1157a708ede6ec68c5c9fe5
andreasansica <sansica.andrea99@gmail.com>
2020-12-11 12:33:58 +0100

    aggiustamenti footer

--- js/script.js
+++ js/script.js
@@ -46,57 +46,110 @@ var app = new Vue ({
     ],
 
     //  FOOTER
-    colonne: [
+    footer1: [
       {
-        testo:[ 'AVADA UNIVERSITY' ,
-                'Residential Location 9521 Broadsberry Avenue       Paddington RC7 9ZA' ,
-                '1.800.458.556',
-                'info@yourdomail.com',
-                'Monday-Friday: 8:00 AM -10:00 PM'
+        testo:[
+            'AVADA UNIVERSITY' ,
+            'Residential Location 9521 Broadsberry Avenue       Paddington RC7 9ZA' ,
+            '1.800.458.556',
+            'info@yourdomail.com',
+            'Monday-Friday: 8:00 AM -10:00 PM'
               ],
-        icona:['fas fa-home','fas fa-phone' ,'fas fa-envelope', "far fa-clock"]
-      },
+        icona:[
+            'fas fa-home',
+            'fas fa-phone',
+            'fas fa-envelope',
+             "far fa-clock"
+         ]
+      }
+    ],
 
-      {
-        testo:[
-                'RECENT POSTS',
-                'What do successful grads think you should study?',
-                'March 16,2016',
-                'Former student discussed success in the fashion industry',
-                'March 16, 2016',
-                'How do you best prepare for university?',
-                'March 17, 2016'
-              ]
-      },
-      {
-        testo:[
-                'COURSES',
-                'Arts e umanities',
-                'Business e managment',
-                'Engineering & technology',
-                'Life science & Medicine',
-                'Natural science'
-              ]
-      },
-      {
-        testo:[
-                'RESOURCES',
-                'Campus Directory',
-                'Academic calendary',
-                'Sport Clubs',
-                'Graduate programs',
-                'Faculty e staff'
-              ]
-      },
 
-      {
-        testo:[
+      footer2:[
+        {
+          testo:[
+              'RECENT POSTS',
+              'What do successful grads think you should study?',
+              'March 16,2016',
+              'Former student discussed success in the fashion industry',
+              'March 16, 2016',
+              'How do you best prepare for university?',
+              'March 17, 2016'
+                ]
+        },
+      ],
+
+      footer3:[
+        {
+          testo:[
+              'COURSES',
+              'Arts e umanities',
+              'Business e managment',
+              'Engineering & technology',
+              'Life science & Medicine',
+              'Natural science'
+                ]
+        },
+      ],
+      footer4:[
+        {
+          testo:[
+              'ADMISSION',
+              'Eligibility',
+              'Application',
+              'Review Process',
+              'How to apply'
+                ]
+        },
+      ],
+
+      footer5:[
+        {
+          testo:[
+              'RESOURCES',
+              'Campus Directory',
+              'Academic calendary',
+              'Sport Clubs',
+              'Graduate programs',
+              'Faculty e staff'
+                ]
+        },
+      ],
+
+
+      footer6:[
+        {
+          testo:[
+              'QUICKLINES',
+              'University Library',
+              'Campus Servicy',
+              'Careers'
+                ]
+        }
+      ],
+
+
+      footer7:[
+        {
+          testo:[
               'UPCOMING EVENT',
               'Business seminar',
               'July, 1,2019 @ 8.00 am - July 1, 2019 @ 5.00 pm',
               'United States'
-            ]
-      },
-    ]
+              ]
+        },
+      ],
+
+      footer8:[
+        {
+          testo:[
+              'POLITICS SEMINAR',
+              'November 6, 2019 @8:00 am - November 6, 2021 @ 5:00 pm',
+              'United Kingdom',
+              'View More'
+                ]
+        }
+      ]
+
   }
 })
